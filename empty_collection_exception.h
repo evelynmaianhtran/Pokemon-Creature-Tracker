7a1c17e9e37a3642d5eb88d5a112b50886f88e6c
evelynmaianhtran <140688900+evelynmaianhtran@users.noreply.github.com>
2025-06-13 15:38:23 +1000

    Update empty_collection_exception.h

--- empty_collection_exception.h
+++ empty_collection_exception.h
@@ -1,10 +1,6 @@
 /*
 * empty_collection_exception.h
-* Written by : SENG1120 Staff (c1234567)
-* Modified : 05/10/2023 - the signature was updated to correctly override the what() method from std::exception
-*
 * This class represents a custom exception that should be thrown when a collection is empty.
-* This file should be used in conjunction with Assignment 3 for SENG1120.
 */
 #ifndef SENG1120_EMPTY_COLLECTION_H
 #define SENG1120_EMPTY_COLLECTION_H
